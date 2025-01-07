453e9ea396cd90af23f512a575cc58c6b8d1efa6
p4811910 <p4811910@gmail.com>
2025-01-07 22:15:53 +0530

    Updated build scripts

--- src/main/java/com/my_spring/app/controller/DemoController.java
+++ src/main/java/com/my_spring/app/controller/DemoController.java
@@ -2,13 +2,20 @@ package com.my_spring.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping
 public class DemoController {
 
     @GetMapping
     public String sayHello() {
         return "Hello from api";
+    }
+
+    @GetMapping("/db")
+    public String sayHelloConnectToDb() {
+        return "API is connected to db";
     }
 
 }
